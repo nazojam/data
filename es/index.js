bbdecd0a1d6668960cd61e8ec270110e3666d2b9
まどの <madonoharu@gmail.com>
2019-05-20 23:17:58 +0900

    add es

---
+++ es/index.js
@@ -0,0 +1,11 @@
+export { default as api_mst_ship, MstAbysallShip, MstAllyShip, MstShip } from '../dist/api_mst_ship';
+export { default as api_mst_stype } from '../dist/api_mst_stype';
+export { default as api_mst_slotitem, MstEquipment } from '../dist/api_mst_slotitem';
+export { default as api_mst_slotitem_equiptype } from '../dist/api_mst_slotitem_equiptype';
+export { default as api_mst_equip_ship } from '../dist/api_mst_equip_ship';
+export { default as api_mst_equip_exslot } from '../dist/api_mst_equip_exslot';
+export { default as api_mst_equip_exslot_ship } from '../dist/api_mst_equip_exslot_ship';
+export { default as api_mst_shipgraph } from '../dist/api_mst_shipgraph';
+export { default as ships, ShipData, ShipStat } from '../dist/ships';
+export { default as improvements } from '../dist/improvements';
+export { default as shipFaceRects, Rect } from '../dist/shipFaceRects';
