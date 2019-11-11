7e9b56e566458cf1fdd5548419bc6f21518f3794
まどの <madonoharu@gmail.com>
2019-11-11 19:59:17 +0900

    0.6.1

--- scripts/types.ts
+++ scripts/types.ts
@@ -1,7 +1,6 @@
 import fs from "fs"
 import { chain } from "lodash"
 import { api_mst_ship, api_mst_slotitem, MstShip } from "../src"
-import { writeEnum } from "./writeEnum"
 
 const isPlayerShip = (ship: MstShip) => ship.api_id <= 1500
 const isAbyssalShip = (ship: MstShip) => !isPlayerShip(ship)
@@ -10,14 +9,33 @@ const dataChain = chain(api_mst_ship)
   .sortBy("api_sort_id")
   .map(ship => {
     const { api_id, api_name: name, api_sort_id } = ship
-    const grade = api_sort_id % 10
-    const kind = (api_sort_id - grade) / 10
-    return { shipId: api_id, name, grade, kind }
+    const rank = api_sort_id % 10
+    const individual = (api_sort_id - rank) / 10
+    return { shipId: api_id, name, rank, individual }
   })
 
-const createLiteralType = (typeName: string, types: string[]) => {
+export const createLiteralType = (typeName: string, types: string[]) => {
   const inner = types.map(type => `  | "${type}"`).join("\r\n")
   return `export type ${typeName} =\r\n${inner}\r\n`
+}
+
+export const createEnum = (enumName: string, data: (readonly [string, number])[], isConst = true) => {
+  const names = data.map(([name]) => name)
+  const duplicatedNames = names.filter(name => names.indexOf(name) !== names.lastIndexOf(name))
+  const inner = data
+    .map(([name, value]) => {
+      const key = duplicatedNames.includes(name) ? `${name} id${value}` : name
+      const line = `  "${key}" = ${value}`
+      return line
+    })
+    .join(",\r\n")
+
+  return `export${isConst ? " const" : ""} enum ${enumName} {\r\n${inner}\r\n}\r\n`
+}
+
+export const writeEnum = (fileName: string, ...params: Parameters<typeof createEnum>) => {
+  const text = createEnum(...params)
+  return fs.promises.writeFile(fileName, text)
 }
 
 export const writeShipName = () => {
@@ -38,14 +56,14 @@ export const writeShipName = () => {
 
   const text = `${playerShipType}\r\n${abyssalShipType}\r\nexport type ShipName = PlayerShipName | AbyssalShipName\r\n`
 
-  fs.writeFile("src/ShipName.ts", text, console.error)
+  return fs.promises.writeFile("src/ShipName.ts", text)
 }
 
 export const writeGearName = () => {
   const gearNames = api_mst_slotitem.map(({ api_name }) => api_name)
   const text = createLiteralType("GearName", gearNames)
 
-  fs.writeFile("src/GearName.ts", text, console.error)
+  return fs.promises.writeFile("src/GearName.ts", text)
 }
 
 export const writeAbyssalShipClass = () => {
@@ -70,7 +88,7 @@ export const writeAbyssalShipClass = () => {
   const specialClassMap = specialClasses.map((name, index) => [name, 2001 + index] as [string, number])
   const classMap = irohaClassMap.concat(specialClassMap)
 
-  writeEnum("src/AbyssalShipClass.ts", "AbyssalShipClass", classMap, false)
+  return writeEnum("src/AbyssalShipClass.ts", "AbyssalShipClass", classMap, false)
 }
 
 export const writeShipId = () =>
@@ -78,8 +96,21 @@ export const writeShipId = () =>
 export const writeGearId = () =>
   writeEnum("src/GearId.ts", "GearId", api_mst_slotitem.map(({ api_name, api_id }) => [api_name, api_id]))
 
-writeAbyssalShipClass()
-writeShipName()
-writeGearName()
-writeShipId()
-writeGearId()
+export const writeRemodelGroup = () => {
+  const data = dataChain
+    .filter(ship => ship.shipId < 1500 && ship.rank === 1)
+    .uniqBy("individual")
+    .map(ship => [ship.name, ship.shipId] as const)
+    .value()
+
+  return writeEnum("src/RemodelGroup.ts", "RemodelGroup", data)
+}
+
+export const writeTypes = async () => {
+  await writeAbyssalShipClass()
+  await writeShipName()
+  await writeGearName()
+  await writeShipId()
+  await writeGearId()
+  await writeRemodelGroup()
+}
